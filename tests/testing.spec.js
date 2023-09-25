05af8fdf4e4b1b5290569237824dfbeba989b0de
Balram Mandal <mandalbalram12@gmail.com>
2023-09-25 12:20:05 +0530

    Deleted Unneccessary Files

--- tests/testing.spec.js
+++ tests/testing.spec.js
@@ -1,12 +1,12 @@
 // @ts-check
 const { test} = require('@playwright/test');
-import loginObj from '../pagesClickup/dummy';
+// import loginObj from '../pagesClickup/dummy';
 
 test('Fetching the Locator', async ({}) => {
 
   // const login = new loginPage01(page);
   
-  await loginObj.navigate();
-  await loginObj.enterCredentials();
+  // await loginObj.navigate();
+  // await loginObj.enterCredentials();
 
 });
